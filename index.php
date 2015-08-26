19da0c86a598d17303a80ee89acd2ab292c44171
Helder <helder@lamoulade.com>
2015-08-26 18:24:50 +0100

    introduced the hammerjs library. added the viewport meta

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
 	<link rel="stylesheet" href="css/app.css">
 </head>
 <body>
@@ -40,7 +41,8 @@
 
 	</div>
 
-	<script src="js/app.js"></script>
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.4/hammer.min.js"></script>
+	<script type="text/javascript" src="js/app.js"></script>
 
 </body>
 </html>
